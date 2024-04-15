13a9d0a0dfe905a1d9fab6f5139d81a09c3f014a
Ezemilton <ezemilton@hotmil.com>
2024-04-14 23:53:43 -0300

    database postgresql

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_bcrypt import Bcrypt
+import os
 
 app = Flask(__name__)
 #if os.getenv("DEBUG") == 0:
@@ -10,7 +11,7 @@ app = Flask(__name__)
 #else:
 #    link_banco = "sqlite://comunidade.db"
 #Variável que define o endereço do banco de dados
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 #Variável que define a criptografia secreta
 app.config["SECRET_KEY"] = "af78cfa9a6d28478f96e74181f86fa3"
 #Variável que define o endereço das fotos postadas
